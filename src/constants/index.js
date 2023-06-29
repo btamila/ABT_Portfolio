e2e439c3f3b9c874f0f9208baecfefc839171b48
GMX\002TPV744 <bharathi.tamilanban.alagarsamy@ibm.com>
2023-06-29 20:36:28 +0530

    Initial commit

---
+++ src/constants/index.js
@@ -0,0 +1,169 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Web Developer",
+    icon: web,
+  },
+  {
+    title: "Hybrid Mobile Developer (Ionic, COrdova) ",
+    icon: mobile,
+  },
+  {
+    title: "Backend Developer",
+    icon: backend,
+  },
+  {
+    title: "Content Creator",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+];
+
+const experiences = [
+  {
+    title: "Full Stack Developer",
+    company_name: "Firestream World wide",
+    icon: shopify,
+    iconBg: "#383E56",
+    date: "Sep 2011 - Dec 2015",
+    points: [
+      "Developing and maintaining web applications using PHP, Codeigniter, Javascript, Jquery, Angular js, MySqlm HTML, CSS, JSON and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "Marine Delivery App",
+    company_name: "PDI Software",
+    icon: tesla,
+    iconBg: "#E6DEDD",
+    date: "Jan 2016 - Dec 2017",
+    points: [
+      "Developing and maintaining web applications using Ionic Framework, Angular Js, Node Js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews, defect fixing, generate PDF documents and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "React.js Developer",
+    company_name: "PDI Software",
+    icon: starbucks,
+    iconBg: "#383E56",
+    date: "JAN 2017 - NOV 2021",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+
+  {
+    title: "Application Developer",
+    company_name: "IBM",
+    icon: meta,
+    iconBg: "#E6DEDD",
+    date: "Nov 2021 - Present",
+    points: [
+      "Developing and maintaining web applications using Javascript, Typescript, Knockout Js, OJET and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+];
+
+const testimonials = [];
+
+const projects = [];
+
+export { services, technologies, experiences, testimonials, projects };
